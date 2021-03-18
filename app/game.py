27e3102b1d0322e86692ba379a0eced6d0f6167f
pk664 <77949064+pk664@users.noreply.github.com>
2021-03-18 10:07:09 +0700

    reduced complexity if statement

--- app/game.py
+++ app/game.py
@@ -22,23 +22,49 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
+#if u == "rock" and c == "rock":
+#    print("It's a tie!")
+#elif u == "rock" and c == "paper":
+#    print("The computer wins")
+#elif u == "rock" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "paper" and c == "rock":
+#    print("The computer wins")
+#elif u == "paper" and c == "paper":
+#    print("It's a tie!")
+#elif u == "paper" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "scissors" and c == "rock":
+#    print("The computer wins")
+#elif u == "scissors" and c == "paper":
+#    print("The user wins")
+#elif u == "scissors" and c == "scissors":
+#    print("It's a tie!")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+if u == "rock": 
+    if c == "rock": 
+        print("____")
+    elif c == "paper": 
+        print("___")
+    elif c == "scissors": 
+        print("____")
+elif u == "paper": 
+    if c == "rock": 
+        print("____")
+    elif c == "paper": 
+        print("___")
+    elif c == "scissors": 
+        print("____")
+elif u == "scissors": 
+    if c == "rock": 
+        print("____")
+    elif c == "paper": 
+        print("___")
+    elif c == "scissors": 
+        print("____")
+
+# there are even less complex ways of doing this 
+# e.g. using a single dict or tuple approach 
