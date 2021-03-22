8769d294202094e5e65a572ce8cd15816750fa58
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-03-22 17:27:34 -0400

    Simple (#1)
    * Simpler options
    
    * Simple Winner Determination
    
    * Formatted Currencies
    
    * Docstring

--- app/game.py
+++ app/game.py
@@ -5,9 +5,10 @@ from random import choice
 # USER SELECTION
 #
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+choices = ['rock', 'paper', 'scissors']
+u = input(f"Please choose one of {choices}: ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in choices:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -15,30 +16,38 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(choices)
 print("COMPUTER CHOICE:", c)
 
 #
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
+if u == c:
     print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+elif u == "rock" and c == "paper" or u == "paper" and c == "scissors" or u == "scissors" and c == "rock":
+    print("Sorry, the computer won.")
+else:
+    print("Congrats, you win!")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+#if u == "rock" and c == "rock" or u == "paper" and c == "paper" or u == "scissors" and c == "scissors":
+#    print("It's a tie!")
+#elif u == "rock" and c == "paper":
+#    print("The computer wins")
+#elif u == "rock" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "paper" and c == "rock":
+#    print("The computer wins")
+#elif u == "paper" and c == "paper":
+#    print("It's a tie!")
+#elif u == "paper" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "scissors" and c == "rock":
+#    print("The computer wins")
+#elif u == "scissors" and c == "paper":
+#    print("The user wins")
+#elif u == "scissors" and c == "scissors":
+#    print("It's a tie!")
