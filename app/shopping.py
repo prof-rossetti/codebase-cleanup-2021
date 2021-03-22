a7c04ecf745d23ecbc145da02380cea9b1cd3a21
Estelle Spanneut <es1342@georgetown.edu>
2021-03-22 17:22:32 -0400

    Documented the price formatting function

--- app/shopping.py
+++ app/shopping.py
@@ -2,6 +2,21 @@ import os
 from datetime import datetime
 from pandas import read_csv
 
+#FORMAT_USD FUNCTION
+
+def format_usd(my_price): 
+        """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    Param: my_price (int or float) like 4000.444444
+
+    Example: to_usd(4000.444444)
+
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+#ATTRIBUTION: Taken from code provided by Professor Rossetti
+
 # READ INVENTORY OF PRODUCTS
 
 products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
