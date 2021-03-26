6d568e5bbbf6e334c87947c192e3bfebaf8c3c98
pk664 <77949064+pk664@users.noreply.github.com>
2021-03-26 10:58:13 +0700

    Testing (#2)
    * Delete mock_responses.py
    
    We are going to be putting these in the conftest file instead.
    
    * Update conftest.py
    
    Add mock data and commented-out fixture
    
    * Update conftest.py
    
    * Update conftest.py
    
    * Create LICENSE.md
    
    * Test price formatting logic
    
    * Start to test some of the logic
    
    Co-authored-by: Mike Rossetti <s2t2@users.noreply.github.com>

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -2,3 +2,18 @@
 # TODO: import some code
 
 # TODO: test the code
+
+
+# once we have some tests ... 
+# we will run our tests using the pytest package 
+# pytest needs to be installed via pip just like any other third party package 
+# pytest is already listed in the requirements.txt file 
+
+from app.shopping import format_usd
+# this lets us import the function we dfined from the application shopping cart 
+# the reason we can do this is because of the presence of the __init__.py file 
+# this allows us to import functionality 
+# the presence of this file helps us import code from one python file to another 
+
+def test_format_usd():
+    assert format_usd(9.5) == "$9.50"
