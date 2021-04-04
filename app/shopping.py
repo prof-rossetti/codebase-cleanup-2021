8ae381f5a38a5966431c3160f54dab78f722460e
Paul Hadchiti <pah73@georgetown.edu>
2021-04-04 19:56:14 -0400

    trying pull request

--- app/shopping.py
+++ app/shopping.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from pandas import read_csv
 
-#USD FORMATTING FUNCTION
+#USD FORMATTING FUNCTION ()
 def format_usd(my_price):
     """
     Formats a number as USD with dollar sign and two decimals (and also thousands separator)
