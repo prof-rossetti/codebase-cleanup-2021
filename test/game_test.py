bac7db11fd5233fa314ad9e27ddac1ef99d6af16
carlodwek <carlodwek@gmail.com>
2021-04-05 18:30:18 -0400

    game fix 3

--- test/game_test.py
+++ test/game_test.py
@@ -5,3 +5,19 @@ from app.game import determine_winner
 def test_determine_winner():
     result = determine_winner("rock","rock")
     assert result == "It's a tie!"
+    result = determine_winner("rock","paper")
+    assert result == "The user wins"
+    result = determine_winner("rock","scissors")
+    assert result == "The computer wins"
+    result = determine_winner("paper","rock")
+    assert result == "The computer wins"
+    result = determine_winner("paper","paper")
+    assert result == "It's a tie!"
+    result = determine_winner("paper","scissors")
+    assert result == "The user wins"
+    result = determine_winner("scissors","rock")
+    assert result == "The user wins"
+    result = determine_winner("scissors","paper")
+    assert result == "The computer wins"
+    result = determine_winner("scissors","scissors")
+    assert result == "It's a tie!"
