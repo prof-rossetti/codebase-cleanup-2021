3ebb2674355649536bcb788e9580d9c8128ded26
Antonio Griffith-Keaton <antoniogriffith2@yahoo.com>
2021-04-05 08:59:06 -0400

    Update game.py

--- app/game.py
+++ app/game.py
@@ -5,6 +5,30 @@ from random import choice
 # USER SELECTION
 #
 
+def determineWinner(userChoice, ranChoice):
+    
+    if u == "rock" and c == "rock":
+        print("It's a tie!")
+    elif u == "rock" and c == "paper":
+        print("The computer wins")
+    elif u == "rock" and c == "scissors":
+        print("You've won!")
+
+    elif u == "paper" and c == "rock":
+        print("The computer wins")
+    elif u == "paper" and c == "paper":
+        print("It's a tie!")
+    elif u == "paper" and c == "scissors":
+        print("You win!")
+
+    elif u == "scissors" and c == "rock":
+        print("The computer wins")
+    elif u == "scissors" and c == "paper":
+        print("You win!")
+    elif u == "scissors" and c == "scissors":
+        print("It's a tie!")
+
+
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
 if u not in ["rock", "paper", "scissors"]:
@@ -21,24 +45,4 @@ print("COMPUTER CHOICE:", c)
 #
 # DETERMINATION OF WINNER
 #
-
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
-
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
-
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+determineWinner(u, c)
