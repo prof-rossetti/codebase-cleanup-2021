78cabd2fb352c5ac93b9425f76b0e062bf1a242b
carlodwek <carlodwek@gmail.com>
2021-04-05 18:51:51 -0400

    game fix 6

--- app/game.py
+++ app/game.py
@@ -6,23 +6,20 @@ def determine_winner(c,u):
     if u == c:
         out = "It's a tie!"
 
-    elif u == "rock":
-        if c == "paper":
-            out = "The computer wins"
-        elif c == "scissors":
-            out = "The user wins"
+    elif u == "rock" and c == "paper":
+        out = "The computer wins"
+    elif u == "rock" and c == "scissors":
+        out = "The user wins"
 
-    elif u == "paper":
-        if c == "scissors":
-            out = "The computer wins"
-        elif c == "rock":
-            out = "The user wins"
+    elif u == "paper" and c == "scissors":
+        out = "The computer wins"
+    elif u == "paper" and c == "rock":
+        out = "The user wins"
 
-    elif u == "scissors":
-        if c == "rock":
-            out = "The computer wins"
-        elif c == "paper":
-            out = "The user wins"
+    elif u == "scissors" and c == "rock":
+        out = "The computer wins"
+    elif u == "scissors" and c == "paper":
+        out = "The user wins"
 
     return out
 
