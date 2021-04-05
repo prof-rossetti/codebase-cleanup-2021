1a1d117f4c1ff3545d5d8c604fb9b46f9035ac17
cb1521 <77860675+cb1521@users.noreply.github.com>
2021-04-05 13:01:19 -0400

    Update game.py (#4)

--- app/game.py
+++ app/game.py
@@ -23,23 +23,23 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
+if u == c:
     print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
+elif u == "rock":
+    if c == "paper":
+        print("The computer wins")
+    elif c == "scissors":
+        print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+elif u == "paper": 
+    if c == "rock":
+        print("The computer wins")
+    elif c == "scissors":
+        print("The user wins")
+
+elif u == "scissors":
+    if c == "rock":
+        print("The computer wins")
+    elif c == "paper":
+        print("The user wins")
