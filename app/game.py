f1727a9d2acf25749a846045d9efb445c4282275
carlodwek <carlodwek@gmail.com>
2021-04-05 18:23:21 -0400

    game fixes 2

--- app/game.py
+++ app/game.py
@@ -2,48 +2,52 @@
 from random import choice
 
 def determine_winner(c,u):
+    w = "The user wins"
+    l = "The computer wins"
+
     if u == c:
-        print("It's a tie!")
+        return "It's a tie!"
 
     elif u == "rock":
         if c == "paper":
-            print("The computer wins")
+            return l
         elif c == "scissors":
-            print("The user wins")
+            return w
 
     elif u == "paper":
         if c == "scissors":
-            print("The computer wins")
+            return l
         elif c == "rock":
-            print("The user wins")
+            return w
 
     elif u == "scissors":
         if c == "rock":
-            print("The computer wins")
+            return l
         elif c == "paper":
-            print("The user wins")
+            return w
 
 
 #
 # USER SELECTION
 #
+if __name__ == "__main__":
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    choices = ["rock", "paper", "scissors"]
+    if u not in choices:
+        print("OOPS, TRY AGAIN")
+        exit()
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-choices = ["rock", "paper", "scissors"]
-if u not in choices:
-    print("OOPS, TRY AGAIN")
-    exit()
+    #
+    # COMPUTER SELECTION
+    #
 
-#
-# COMPUTER SELECTION
-#
+    c = choice(choices)
+    print("COMPUTER CHOICE:", c)
 
-c = choice(choices)
-print("COMPUTER CHOICE:", c)
+    #
+    # DETERMINATION OF WINNER
+    #
 
-#
-# DETERMINATION OF WINNER
-#
-
-determine_winner(c,u)
+    result = determine_winner(c,u)
+    print(result)
--- test/game_test.py
+++ test/game_test.py
@@ -1,4 +1,7 @@
 
 # TODO: import some code
-
+from app.game import determine_winner
 # TODO: test the code
+def test_determine_winner():
+    result = determine_winner("rock","rock")
+    assert result == "It's a tie!"
