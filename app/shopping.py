1c405ae2a90d6324ae36e3c5512ac625778103c9
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-04-05 16:51:21 -0400

    Shopping refactoring and testing

--- app/shopping.py
+++ app/shopping.py
@@ -10,6 +10,19 @@ def format_usd(my_price):
     """
     return f"${my_price:,.2f}"
 
+#this part before if __name__ section is from https://github.com/s2t2/codebase-cleanup-2021/pull/5/commits/26b96b5ca7ac0217a5c71e9c0f85578af3c38d03
+def lookup_product(product_id, all_products):
+    """
+    Params :
+        product_id (str) like "8"
+        all_products (list of dict) each dict should have "id", "name", "department", "aisle", and "price" attributes
+    """
+    matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+    if any(matching_products):
+        return matching_products[0]
+    else:
+        return None
+
 
 # Prevent all of the app code from being imported
 # But still be able to run it from the command line:
@@ -29,11 +42,11 @@ if __name__ == "__main__":
       if selected_id.upper() == "DONE":
           break
       else:
-          matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-          if any(matching_products):
-              selected_products.append(matching_products[0])
-          else:
-              print("OOPS, Couldn't find that product. Please try again.")
+         matching_product = lookup_product(selected_id, products)
+         if matching_product:
+             selected_products.append(matching_product)
+         else:
+             print("OOPS, Couldn't find that product. Please try again.")
 
     checkout_at = datetime.now()
     subtotal = sum([float(p["price"]) for p in selected_products])
--- test/shopping_test.py
+++ test/shopping_test.py
@@ -2,8 +2,31 @@
 # only works because of the __init__.py file 
 # there is no code there but its presence help import files among files
 
-from app.shopping import format_usd
+import os
+from pandas import read_csv
+from app.shopping import format_usd, lookup_product
 
 def test_format_usd():
     assert format_usd(9.5) == "$9.50"
     
+
+
+# this section is from https://github.com/s2t2/codebase-cleanup-2021/pull/5/commits/26b96b5ca7ac0217a5c71e9c0f85578af3c38d03
+# consider making this a fixture!
+mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
+mock_products_df = read_csv(mock_products_filepath)
+mock_products = mock_products_df.to_dict("records")
+
+def test_lookups():
+    # with valid product id, returns the product info:
+    valid_result = lookup_product("8", mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': 8,
+        'name': 'Product 8',
+        'price': 10.0
+    }
+    # with invalid product id, returns None:
+    invalid_result = lookup_product("88888888", mock_products)
+    assert invalid_result == None
