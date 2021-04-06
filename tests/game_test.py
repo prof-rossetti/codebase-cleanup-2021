7fb201bdd299d934355519349688b6fb6752549e
cassiehenning <cjh281@georgetown.edu>
2021-04-06 08:51:23 -0400

    game_test finalized

--- tests/game_test.py
+++ tests/game_test.py
@@ -3,17 +3,17 @@
 from app.game import determine_winner
 
 def test_determine_winner(): 
-    determine_winner("rock", "rock") == None 
-    determine_winner("rock", "paper") == "paper"
-    determine_winner("rock", "scissors") == "rock"
+    assert determine_winner("rock", "rock") == None 
+    assert determine_winner("rock", "paper") == "paper"
+    assert determine_winner("rock", "scissors") == "rock"
 
-    determine_winner("paper", "rock") == "paper"
-    determine_winner("paper", "paper") == None 
-    determine_winner("paper", "scissors") == "scissors" 
+    assert determine_winner("paper", "rock") == "paper"
+    assert determine_winner("paper", "paper") == None 
+    assert determine_winner("paper", "scissors") == "scissors" 
 
-    determine_winner("scissors", "rock") == "rock"
-    determine_winner("scissors", "paper") == "scissors"
-    determine_winner("scissors", "scissors") == None 
+    assert determine_winner("scissors", "rock") == "rock"
+    assert determine_winner("scissors", "paper") == "scissors"
+    assert determine_winner("scissors", "scissors") == None 
 
 
 
