838f0bd4d8be2ba8c323b090f53be2bbff4c7221
cassiehenning <cjh281@georgetown.edu>
2021-04-06 09:05:15 -0400

    shopping_test finalized

--- tests/shopping_test.py
+++ tests/shopping_test.py
@@ -17,10 +17,13 @@ import os
 from pandas import read_csv
 from app.shopping import lookup_product
 
+# consider making this a fixture 
 mock_products_filepath = os.path.join(os.path.dirname(__file__),"mock_data", "mock_products.csv")
 mock_products_df = read_csv(mock_products_filepath)
-mock_products = mock_products_df.todict("records")
+mock_products = mock_products_df.to_dict("records")
+
 def test_lookups(): 
+    #with valid product id, returns the product info 
     valid_result = lookup_product("8", mock_products)
     assert valid_result == {
         'aisle': 'Aisle C', 
@@ -30,6 +33,7 @@ def test_lookups():
         'price': 10.0 
     }
 
+    # with valid product id, returns None:
     invalid_result = lookup_product("88888888", mock_products)
     assert invalid_result == None
 
