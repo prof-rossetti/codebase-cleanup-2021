302c102cf2a326ff8612523ffd5e27d6d1ab7c08
victoriaruesch <vr365@georgetown.edu>
2021-04-05 22:11:21 -0400

    Refactored and test product lookup logic

--- app/shopping.py
+++ app/shopping.py
@@ -12,6 +12,18 @@ def format_usd(my_price):
     """
     return f"${my_price:,.2f}"
 
+def lookup_product(product_id, all_products):
+    """
+    Params : 
+        product_id (str) like "8"
+        all_products (list or dict) each dict should have "id", "name", "department", "aisle", and "price" attribute
+    """
+    matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+    if any(matching_products):
+        return matching_products[0]
+    else:
+        return None
+
 #PREVENT ALL THE APP CODE FROM BEING IMPORTED
 #BUT STILL BE ABLE TO RUN IT FROM THE COMMAND LINE IS LIKE THIS...  
 
@@ -31,9 +43,9 @@ if __name__ == "__main__":
         if selected_id.upper() == "DONE":
             break
         else:
-            matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-            if any(matching_products):
-                selected_products.append(matching_products[0])
+            matching_product = lookup_product(selected_id, products)
+            if matching_product:
+                selected_products.append(matching_product)
             else:
                 print("OOPS, Couldn't find that product. Please try again.")
     
