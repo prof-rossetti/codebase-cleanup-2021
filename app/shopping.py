f470eed2e3b0d1fcd6f2bb6a303079ef0939090e
Connor Keyes <76636011+connorkeyes@users.noreply.github.com>
2021-04-07 01:12:37 -0700

    refactored shopping app to remove duplication

--- app/shopping.py
+++ app/shopping.py
@@ -17,9 +17,6 @@ def format_usd(my_price):
 
 if __name__ == "__main__":
 
-
-
-
     # READ INVENTORY OF PRODUCTS
 
     products_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
@@ -44,36 +41,29 @@ if __name__ == "__main__":
 
     subtotal = sum([float(p["price"]) for p in selected_products])
 
-    # PRINT RECEIPT
-
-    print("---------")
-    print("CHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
-    print("---------")
-    for p in selected_products:
-        print("SELECTED PRODUCT: " + p["name"] + "   " + '${:.2f}'.format(p["price"]))
-
-    print("---------")
-    print(f"SUBTOTAL: {format_usd(subtotal)}")
-    print(f"TAX: {format_usd(subtotal * 0.0875)}")
-    print(f"TOTAL: {format_usd((subtotal*0.0875) + subtotal)}")
-    print("---------")
-    print("THANK YOU! PLEASE COME AGAIN SOON!")
-    print("---------")
-
     # WRITE RECEIPT TO FILE
 
     receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
     receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
 
     with open(receipt_filepath, "w") as receipt_file:
-        receipt_file.write("------------------------------------------")
+        receipt_file.write("---------")
+        receipt_file.write("\nCHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
+        receipt_file.write("\n---------")
+
         for p in selected_products:
-            receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+            receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]))
 
         receipt_file.write("\n---------")
-        receipt_file.write(f"\nSUBTOTAL: {subtotal}")
-        receipt_file.write(f"\nTAX: {subtotal * 0.875}")
-        receipt_file.write(f"\nTOTAL: {((subtotal * 0.875) + subtotal)}")
+        receipt_file.write(f"\nSUBTOTAL: {format_usd(subtotal)}")
+        receipt_file.write(f"\nTAX: {format_usd(subtotal * 0.0875)}")
+        receipt_file.write(f"\nTOTAL: {format_usd((subtotal * 0.0875) + subtotal)}")
         receipt_file.write("\n---------")
         receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
         receipt_file.write("\n---------")
+
+    # PRINT RECEIPT
+
+    receipt_file = open(receipt_filepath, 'r')
+    receipt = receipt_file.read()
+    print(receipt)
