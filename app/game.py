d1672dd683f097b01ca23da16d52830ccf991d2e
Kat Huang <katrinaahuang@gmail.com>
2021-04-07 21:24:39 -0400

    Made winner determination less complex

--- app/game.py
+++ app/game.py
@@ -24,23 +24,26 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
-
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
-
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+if u == "rock":
+    if c == "rock":
+        print("Oh, it's a tie.")
+    elif c == "paper":
+        print("Oh, the computer won. It's ok.")
+    elif c == "scissors":
+        print("Oh, you won! Nice job.")
+elif u == "paper":
+    if c == "rock":
+        print("Oh, you won! Nice job.")
+    elif c == "paper":
+        print("Oh, it's a tie.")
+    elif c == "scissors":
+        print("Oh, the computer won. It's ok.")
+elif u == "scissors":
+    if c == "rock":
+        print("Oh, the computer won. It's ok.")
+    elif ce == "paper":
+        print("Oh, you won! Nice job.")
+    elif c == "scissors":
+        print("Oh, it's a tie.")
+else:
+    print("OOPS, SOMETHING WENT WRONG. Please try again.")
